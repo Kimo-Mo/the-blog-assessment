0fdb01215b958a242ecec02bd5386cad82a4e6a5
kareem mohamed <kimomo844@gmail.com>
2025-06-28 20:33:52 +0300

    Add Loading component and PostDetailPage for dynamic blog post rendering

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,66 @@
+type Props = { params: { id: string } };
+
+import { getMockedPostById } from "@/app/lib/getMockedPosts";
+import { Metadata } from "next";
+import Image from "next/image";
+import { Post } from "@/app/types/post";
+import { notFound } from "next/navigation";
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = await params;
+  const post = await getMockedPostById(Number(id));
+  if (!post) {
+    return { title: "Post Not Found" };
+  }
+
+  return {
+    title: post.title,
+    description: post.body.substring(0, 160),
+  };
+}
+
+export default async function PostDetailPage({ params }: Props) {
+  const { id } = await params;
+  const post: Post = await getMockedPostById(Number(id));
+
+  if (!post) return notFound();
+
+  return (
+    <main className="pb-4">
+      <p className="text-[#6941C6]">
+        {post.author} • {post.date}
+      </p>
+      <h2 className="font-semibold text-2xl mb-8 mt-4">{post.title}</h2>
+      <Image
+        priority={false}
+        loading="lazy"
+        src={post.image}
+        alt={post.title}
+        width={800}
+        height={400}
+        className="w-full h-80 sm:h-90 object-cover mb-6"
+      />
+      <div className="flex flex-col gap-3 my-4">
+        <p className="text-[#667085] dark:text-[#c0c5d0]">{post.body}</p>
+        <p className="text-[#667085] dark:text-[#c0c5d0]">{post.body}</p>
+      </div>
+      <Image
+        priority={false}
+        loading="lazy"
+        src={post.image}
+        alt={post.title}
+        width={800}
+        height={400}
+        className="w-full h-80 sm:h-90 object-cover mb-6"
+      />
+      <div className="flex flex-col gap-3 my-4">
+        <p className="text-[#667085] dark:text-[#c0c5d0]">{post.body}</p>
+        <p className="text-[#667085] dark:text-[#c0c5d0]">{post.body}</p>
+        <p className="text-[#667085] dark:text-[#c0c5d0]">{post.body}</p>
+        <span className="text-[#6941C6] py-[2px] px-[10px] bg-[#F9F5FF] rounded-2xl w-fit">
+          {post.category}
+        </span>
+      </div>
+    </main>
+  );
+}
