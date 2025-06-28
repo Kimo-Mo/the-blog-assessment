11945e7f6d95f0ec494711075beb2cef9a7a5fed
kareem mohamed <kimomo844@gmail.com>
2025-06-28 20:34:24 +0300

    Add ErrorPage component for handling loading errors and update layout with NavBar

---
+++ src/app/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { useEffect } from "react";
+import ErrorMessage from "@/app/components/ErrorMessage";
+
+export default function ErrorPage({ error }: { error: Error }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <ErrorMessage message="Failed to load blog posts. Please try again." />
+  );
+}
