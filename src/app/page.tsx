c1d42f8d6427ad3fe1e51458cd8782b1e8234d01
kareem mohamed <kimomo844@gmail.com>
2025-06-29 03:14:24 +0300

    Refactor Home component to handle search parameters and pass currentPage to AllBlogPosts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 import Hero from "@/app/components/Hero";
 import RecentPosts from "@/app/components/RecentPosts";
 import AllBlogPosts from "@/app/components/AllBlogPosts";
+import Link from "next/link";
 
-export default function Home() {
-
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ page?: string }>;
+}) {
+  const searchParamsAwaited = await searchParams;
+  const currentPage = Number(searchParamsAwaited.page) || 1;
   return (
     <main>
       <Hero />
       <RecentPosts />
-      <AllBlogPosts />
+      <AllBlogPosts currentPage={currentPage} />
+      <Link
+        href="/posts"
+        className="block w-fit my-4 mx-auto bg-[var(--foreground)] text-[var(--background)] py-2 px-4 rounded-md ">
+        All Posts
+      </Link>
     </main>
   );
 }
